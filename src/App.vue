926534a3880aca0e3971abd53878815acbda2f51
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-03-29 17:37:28 +0300

    add vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+  export default {
+      data(){
+        return {
+          message: 'Hello World'
+        }
+      }
+  }
+</script>
+
+<template>
+  <input type="text" v-model="message"> 
+  <button v-on:click="message='Goodbye World';">Click me!</button>
+  <h1>{{ message }}</h1>
+  <h2>{{ message.split('').reverse().join('') }}</h2>
+</template>
+
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// let text = document.getElementById('text');
+// console.log(text.innerHTML);
+// setTimeout(function () {
+//     text.innerHTML = 'Goodbye World';
+// }, 5000);
+
+// let button = document.getElementById('button');
+
+// button.addEventListener('click', function (){
+//     text.innerHTML = 'Goodbye Cruel World!';
+// });
